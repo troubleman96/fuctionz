1bd58aeda80f328b1cb9721827e680f3a5d7703c
troubleman96 <itslugenge96@gmail.com>
2024-11-23 00:26:36 +0300

    1.0

---
+++ Functionz/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Functions{
+    
+    class Program{
+
+        public void display()
+        {
+            Console.WriteLine("wat is the time");
+        }
+
+        public void showMessage(string message)
+        {
+            Console.WriteLine("Hello, " + message);
+        }
+
+        int addNumber(int a, int b)
+        {
+            int sum = a + b;
+            return sum;
+        }
+        
+        public int mathFunc()
+        {
+            int x = 12, y = 12, sum1;
+            sum1 = x + y;
+            return sum1;
+        }
+
+        static void Main(string[] arg)
+        {
+            Program uno = new Program();
+            Program seco = new Program();
+            
+            seco.showMessage("mambo ni vp?");
+
+            int sum = seco.addNumber(3, 5);
+
+            Console.WriteLine("the sum is: " + sum);
+
+            Console.WriteLine();
+
+            seco.mathFunc();
+
+            Console.WriteLine("the total is: " + seco.mathFunc() );
+            
+            uno.display();
+        }   
+    }
+    
+}
